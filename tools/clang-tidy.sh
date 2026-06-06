08d38fe3daf1a0530dc18be9403786c3a42d323a
Arpad Balind <balind.arpad@yandex.ru>
2026-06-06 09:30:49 +0200

    SC Slicing to files and folders added to the tidy script and target

--- tools/clang-tidy.sh
+++ tools/clang-tidy.sh
@@ -5,9 +5,6 @@ set -e
 help() {
     cat <<EOF >&2
 Usage: ${0} <path>
-
-Options:
-    <path> A path where a compile_commands.json is located
 EOF
 }
 
@@ -23,16 +20,29 @@ if [ ! -d "${path}" ]; then
     exit 1
 fi
 
-shift
-files=$*
+scope="${SRC}"
 
-if [ -z "${files}" ]; then
+echo "scope=<${scope}>"
+
+if [ -n "${scope}" ]; then
+    if [ -d "${scope}" ]; then
+        files=$(find "${scope}" -type f \( -name '*.c' -o -name '*.cpp' \))
+    elif [ -f "${scope}" ]; then
+        files="${scope}"
+    else
+        echo "Error: ${scope} does not exist" >&2
+        exit 1
+    fi
+else
     files=$(git ls-files -co --exclude-standard | grep '\.\(c\|cpp\)$')
 fi
+
+echo "ARPY_FILES:"
+printf '%s\n' ${files}
 
 output=$(mktemp -tu tidy.XXXXXX)
 trap 'rm -f "${output}"' EXIT
 mkfifo "${output}"
 
-cat< "${output}" | sed -E '/^clang-tidy/d' | sed 's/clang-tidy.*//' &
+cat < "${output}" | sed -E '/^clang-tidy/d' | sed 's/clang-tidy.*//' &
 echo "${files}" | xargs run-clang-tidy -quiet -p "${path}" > "${output}"
